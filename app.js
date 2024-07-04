311241fd76060ecf83f1af724b8d6d79fb67d51b
ahmedanis45 <ahmedanis4546@gmail.com>
2024-07-04 15:45:33 +0500

    ..

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ let getUser = () => {
 
 let func = () => {
     
-    let suser = document.getElementById("suser");
+    let suser = document.getElementById("susername");
     let semail = document.getElementById("semail");
     let spass = document.getElementById("spassword");   
     if(suser.value.trim().length != 0 && semail.value.trim().length != 0 && spass.value.trim().length != 0)
@@ -67,6 +67,7 @@ let login =  () => {
         {
             console.log(val);
             alert("login success");
+            location.href = "admin.html";
         }
     })
 
@@ -81,9 +82,114 @@ if(signUp)
 {
     signUp.addEventListener("click", func)
 }
-else
+else if(ladmin)
 {
     ladmin.addEventListener("click", login);
 }
 // let sadmin = document.getElementById("sadmin");
 // sadmin.addEventListener("click", func)
+
+
+
+// Cart Section
+let addItem = document.getElementById("addItem");
+if(addItem)
+{
+    let cartArr = [];
+    let fastFoodSec = document.getElementById("fastFoodSec");
+    addItem.addEventListener("click", () =>{
+        let prodImg = document.getElementById("prodImg");
+        let prodTitle = document.getElementById("prodTitle");
+        let prodReview = document.getElementById("prodReview");
+        let prodOrders = document.getElementById("prodOrders");
+        let prodPrice = document.getElementById("prodPrice");
+        let prodDes = document.getElementById("prodDes");
+
+        let cart = {
+            img: prodImg.value,
+            title: prodTitle.value,
+            review: prodReview.value,
+            orders: prodOrders.value,
+            price: prodPrice.value,
+            des: prodDes.value,
+        }
+        cartArr.push(cart);
+
+        localStorage.setItem("storeCardData", JSON.stringify(cartArr));
+    })
+    let getCartData = localStorage.getItem("storeCardData");
+    if(getCartData != null)
+    {
+        cartArr = JSON.parse(getCartData);
+        console.log(cartArr);
+        cartArr.filter((cartVal) =>{
+            console.log(cartVal);
+            let div1 = document.createElement("div");
+            div1.setAttribute("class", "homeCardSec rounded mb-2")
+            let div01 = document.createElement("div");
+            div01.setAttribute("class", "cardImg");
+            let image1 = document.createElement("img");
+            image1.setAttribute("class", "rounded h-100 w-100");
+            image1.src = cartVal.img;
+            div01.appendChild(image1);
+            div1.appendChild(div01);
+            fastFoodSec.appendChild(div1);
+
+            let divs = document.createElement("div")
+            let div2 = document.createElement("div"); //888
+            div2.setAttribute("class", "d-flex px-2 mt-2")
+            let title = document.createElement("p")
+            title.setAttribute("class", "px-1 fw-bold mb-1")
+            let paraTitle = document.createTextNode(cartVal.title)
+            title.appendChild(paraTitle); 
+            div2.appendChild(title)
+            let span1 = document.createElement("span");
+            span1.setAttribute("class", "px-1");
+            let span1Text = document.createTextNode(cartVal.review);
+            span1.appendChild(span1Text);
+            div2.appendChild(span1)
+            let span2 = document.createElement("span")
+            let span2Text = document.createTextNode(cartVal.orders);
+            span2.appendChild(span2Text);
+            div2.appendChild(span2)
+            divs.appendChild(div2)
+            div1.appendChild(divs)
+            
+            let paraDes = document.createElement("p")
+            paraDes.setAttribute("class", "px-2 my-2");
+            let paraDesText = document.createTextNode(cartVal.des)
+            paraDes.appendChild(paraDesText);
+            divs.appendChild(paraDes)
+
+            let div3 = document.createElement("div");
+            div3.setAttribute("class", "d-flex justify-content-between px-2 mb-1")
+            let para2 = document.createElement("p")
+            para2.setAttribute("class", "fw-bold");
+            let para2Text = document.createTextNode(cartVal.price)
+            para2.appendChild(para2Text);
+            div3.appendChild(para2)
+            let span3 = document.createElement('span')
+            span3.setAttribute("class", "addtoCartimg rounded border")
+            let span3Text = document.createTextNode("Add")
+            span3.appendChild(span3Text);
+            div3.appendChild(span3)
+            let icon = document.createElement("i")
+            icon.setAttribute("class", "text-white pe-0 fa-solid fa-bag-shopping")
+            span3.appendChild(icon);
+            div3.appendChild(span3);
+            divs.appendChild(div3)
+
+
+        })
+    }
+
+}
+
+let goHomePage = document.getElementById("goHome");
+if(goHomePage)
+{
+    goHomePage.addEventListener("click", () => {
+        location.href = "home.html";  
+    })
+}
+
--- cart.js
+++ cart.js
@@ -1,9 +1,68 @@
-var num = document.getElementById("num")
+document.addEventListener("DOMContentLoaded", () => {
+    let addToCart = document.getElementById("addToCart");
+    if(addToCart) {
+        addToCart.addEventListener("click", () => {
+            let cartNo = document.getElementById("cartNo");  
+            let cartImg = document.getElementById("cartImg");
+            let cartName = document.getElementById("cartName");
+            let cartPrice = document.getElementById("cartPrice");
+
+            let carts = [];
+
+            let cart = {
+                img: cartImg.src,
+                name: cartName.innerText,
+                price: cartPrice.innerText,
+            }
+            carts.push(cart);
+            localStorage.setItem("cartData", JSON.stringify(carts));
+            cartNo = document.getElementById("cartNo");
+            cartNo.innerText = carts.length;
+        });
+
+        let getCart = localStorage.getItem("cartData");
+        if(getCart != null) {
+            let carts = JSON.parse(getCart);
+            console.log(carts);
+            let show = document.getElementById("showAddToCart");
+            carts.map((val) => {
+                console.log(val);
+                show.innerHTML += `
+                <div class="img5">
+                    <img class="h-100 w-100" src="${val.img}" alt="">
+                </div>
+                <p class="cartp">${val.name}</p>
+                <p class="cartp">${val.price} Rs</p>
+                <div class="d-flex flex-md-row">
+                    <div class="d-flex cartbtns">
+                        <button id="minusbtn" class="cartbtn">-</button>
+                        <p id="num">0</p>
+                        <button id="plusbtn" class="cartbtn">+</button>
+                    </div>
+                    <button class="cartbtn">
+                        <img class="ps-3" src="./images/trash-2.svg" alt="">
+                    </button>
+                </div>`;
+            });
+        }
+    }
+});
+
+
+
+
+
+
+let totalItemOrder =  document.getElementById("totalItemOrder");
+let totalCost = document.getElementById("totalCost");
+var num = document.getElementById("num");
 var plusbtn = document.getElementById("plusbtn");
 let i = 1;
 if(plusbtn){
     plusbtn.addEventListener("click", () =>{
         num.innerHTML = i;
+        totalItemOrder.innerHTML = i;
+        totalCost.innerHTML = i * 
         i++;
     })
 }
@@ -16,18 +75,25 @@ if(minusbtn){
             {
                 i--;
                 num.innerHTML = i;
+                totalItemOrder.innerHTML = i;
             }
         })
 }
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    const loaderProgress = document.querySelectorAll(".loader-container");
-    let width = 0;
-     let pro = setInterval(() => {
-        width++;
-        loaderProgress[0].style.width = width + "%";
-        loaderProgress[1].style.width = width + "%";
+
+
+
+
+
+if(true){
+    document.addEventListener("DOMContentLoaded", () => {
+        const loaderProgress = document.querySelectorAll(".loader-container");
+        let width = 0;
+        let pro = setInterval(() => {
+            width++;
+        //     loaderProgress[0].style.width = width + "%";
+        // loaderProgress[1].style.width = width + "%";
         if(width >= 100)
             {
                 clearInterval(pro)
@@ -35,3 +101,41 @@ document.addEventListener("DOMContentLoaded", () => {
      }, 100)
      
 })
+}
+
+
+
+
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: true,
+    fade: 'true',
+      grabCursor: true,
+      navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+    },
+  });
+
+
+
